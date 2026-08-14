07bce5850f08cd66c6e7fd0a92a916c79dddf1c1
ujjyant2343-dotcom <ujjyant2343@gmail.com>
2026-08-14 11:59:43 +0530

    Abcd

---
+++ main1.py
@@ -0,0 +1,25 @@
+total_qty=35
+price=100
+revenue= price * total_qty
+print(revenue)
+print("""learaning path:
+       -python basics
+       -data engineering
+       -Ai engineering""")
+
+
+name= input ("enter your name:")
+print ("hello", name)
+
+age= input ("enter your age:")
+print ("your age is", age)
+
+
+# Define a function
+def greet(name):
+    return f"Hello, {name}! Welcome to programming."
+
+# Call the function
+user_name = input("Enter your name: ")
+message = greet(user_name)
+print(message)
---
+++ main2.py
@@ -0,0 +1,24 @@
+"""print("My name is uzi")
+print("uzi loves nature")
+
+name= "uzi"
+language="python"
+print(f"My name is {name}")
+print(f"{name} loves {language}")
+
+print("line1\nline2")
+print("line1\tline2")
+print("path:C\\Users\\ujjya\\Desktop\\python")
+print("she said \"hello\"")
+"""
+
+date = "2024-06-15"
+print(date.replace("-", "/"))
+
+first_name = "uzi"
+last_name = "n"
+print(f"{first_name} {last_name}")
+
+csv="uzi,12,India"
+print(csv.split(","))
+print("="*22)
